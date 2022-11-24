0f11fe4e2e48339e699c4cbc397b1e3380d62a18
Peyman <49761601+technotik2@users.noreply.github.com>
2022-11-23 23:05:41 -0500

    database updates

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const url = "mongodb+srv://peyman:iraniran1394@dbserver.lven6.mongodb.net/?retry
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 var router = express.Router();
 const routes= require('./users')
+var bodyParser = require('body-parser');
 
 const cors = require("cors")
 const { data } = require('jquery');
@@ -64,11 +65,37 @@ MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("mydb");
   var myobj = { Fullname: "Peyman T", Date: "", time:"", Diagnostic:"", Other:"" };
+  
   dbo.collection("customers").insertOne(myobj, function(err, res) {
     if (err) throw err;
     console.log("1 document inserted");
-    db.close();
+    
   });
 });
+
+//not working right
+router.post('/post-data1', function (req, res) {
+  var dbo = client.db("mydb");
+  var bodyJson = JSON.parse(req.body.name)
+  dbo.collection("customers").insertOne(bodyJson, function(err, res) {
+    if (err) throw err;
+    console.log("1 DATA RECIEVED");
+    
+  });
+  
+});
+//redircting user to home page 
+router.post('/post-data1', function (req, res) {
+  res.render('Landing', { title: 'Home' });
+  console.log(req.body.name, req.body.date)
+  
+});
+
+
+
+
+
+
+
 
 module.exports = router;
